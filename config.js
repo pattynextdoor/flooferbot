b5c445ec1fdeb17b4e69aa177bba3641616489ce
patricktumbucon <ptumb001@ucr.edu>
2017-09-15 14:04:09 -0700

    changed interval time back to 30min

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'Hoh6aZxqi8724tP4AXsVz09tL',
-  consumer_secret: 'K2MIefy5jxGh7M16vKMPUcn8w4cqy80lnf1cbSf5SxL6b6ymOq',
-  access_token: '908572056165347328-9e5oCnEFRbgUMRcmKcb6Uh6NXr94zPT',
-  access_token_secret: 'v5uBzQSEb39izoLYpIjCso6xU5nmAou9AVu6X6OdfKUr4'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '-',
+  access_token_secret: ''
 }
